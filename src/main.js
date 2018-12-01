e5f1e87775097b5514a0ecf17debac2857dc65b1
Rémi Vanicat <vanicat@debian.org>
2018-12-01 14:45:18 +0100

    full html5 game: GUI work

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
-var theGame = function (game) {
+var membersBlock
+var stuffBlock
+var enemiesBlock
+var visibleBlock
+var actionBlock
+
+var main = function () {
+  actionBlock = document.getElementById('action')
+  membersBlock = document.getElementById('members')
+  stuffBlock = document.getElementById('stuff')
+  enemiesBlock = document.getElementById('enemies')
+
+  visibleBlock = membersBlock
+  showBlockCreate(actionBlock)()
+
+  document.getElementById('actionSelector').onclick = showBlockCreate(actionBlock)
+  document.getElementById('membersSelector').onclick = showBlockCreate(membersBlock)
+  document.getElementById('stuffSelector').onclick = showBlockCreate(stuffBlock)
+  document.getElementById('enemiesSelector').onclick = showBlockCreate(enemiesBlock)
 }
 
-theGame.prototype = {
-  preload: function () {
-  },
-
-  create: function () {
-    console.log('startirg the game')
+var showBlockCreate = function (block) {
+  return function () {
+    visibleBlock.style.visibility = 'hidden'
+    visibleBlock.style.position = 'absolute'
+    block.style.visibility = 'visible'
+    block.style.position = 'inherit'
+    visibleBlock = block
   }
 }
+/* Export: main */
